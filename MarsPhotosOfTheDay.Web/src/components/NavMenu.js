ccef0f0be0bb23ca42aa29d5b167f0f9095a185e
Saibal Das <saibal_das@hotmail.com>
2020-09-01 10:50:17 -0500

    adding download button to the ReactApp

--- MarsPhotosOfTheDay.Web/src/components/NavMenu.js
+++ MarsPhotosOfTheDay.Web/src/components/NavMenu.js
@@ -31,16 +31,7 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink tag={Link} className="text-dark" to="/mars-rover-images">Mars Rover Images</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/mars-rover-images">Home</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
